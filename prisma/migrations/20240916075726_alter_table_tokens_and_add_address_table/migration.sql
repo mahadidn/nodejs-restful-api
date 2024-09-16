bbf45f2e03dbbdf17b6da751a97de09c4bbc63ab
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2024-09-16 14:59:17 +0700

    fix migration

--- prisma/migrations/20240916075726_alter_table_tokens_and_add_address_table/migration.sql
+++ prisma/migrations/20240916075726_alter_table_tokens_and_add_address_table/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `tokens` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
 
 -- CreateTable
-CREATE TABLE `Address` (
+CREATE TABLE `addresses` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `street` VARCHAR(255) NULL,
     `city` VARCHAR(100) NULL,
@@ -12,7 +12,7 @@ CREATE TABLE `Address` (
     `contact_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE InnoDB;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Address` ADD CONSTRAINT `Address_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
